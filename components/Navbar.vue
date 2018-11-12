7468fa841bf5be8f6882fc9abddfb713ccec37be
rajabiseraji <rajabiseraji@aut.ac.ir>
2018-11-12 14:19:23 +0330

    feat(Navbar): adds CV to the navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,6 +3,13 @@
     <b-navbar sticky toggleable="md" type="dark" variant="primary">
       <!-- <b-navbar-toggle target="nav_collapse"></b-navbar-toggle> -->
       <b-navbar-brand href="#">Mana Poustizadeh</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="/CV.zip">
+          <b-button class="bg-light border-light">
+            CV
+          </b-button>
+        </b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
